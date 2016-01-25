55388922575f56403ef5ccc9701b166385ed07e6
Solrac <solracsiete@gmail.com>
2016-01-25 01:37:30 -0300

    Agregado dependencia Page

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 */
 
 import $ from 'jquery'
+import page from 'page'
 import template from 'src/template'
 import render from 'src/render'
 import * as shows from 'src/api-tvmaze'
@@ -10,25 +11,30 @@ import * as shows from 'src/api-tvmaze'
 
 $(function(){
 
-	if(!localStorage.movies){
-		shows.getShows();
-	} else{
-		render(JSON.parse(localStorage.movies));
-	}
-
-	$(".header").find(".search").on("submit", ev => {
-		ev.preventDefault();
-		let texto = $(".search").find(".search__input").val();
-		if(texto == ""){
-			$(".show").remove();
+	page('/', function(ctx, next){
+		if(!localStorage.movies){
+			shows.getShows();
+		} else{
 			render(JSON.parse(localStorage.movies));
-		}else{
-			$(".show").remove();
-			shows.searchShows();
 		}
-	
 	})
+		
 
+		$(".header").find(".search").on("submit", ev => {
+			ev.preventDefault();
+			let texto = $(".search").find(".search__input").val();
+			if(texto == ""){
+				$(".show").remove();
+				render(JSON.parse(localStorage.movies));
+			}else{
+				$(".show").remove();
+				shows.searchShows();
+			}
+		
+		})
+
+	page();
+	
 })
 
 
